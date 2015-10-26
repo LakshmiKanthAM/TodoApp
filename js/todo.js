201b71a0b9b27165530bf21ffb114fe4edcb4929
Lakshmi Kanth Anakutty Mani <lmani@sapient.com>
2015-10-26 10:21:23 +0530

    first Commit

---
+++ js/todo.js
@@ -0,0 +1,48 @@
+var todo = angular.module('todoApp', []);
+todo.controller('todoController', ['$scope', function($scope) {
+    $scope.todos = [{
+        'title': 'my first task',
+        'done': false
+    }];
+
+    $scope.addTodo = function() {
+        $scope.todos.push({
+            'title': $scope.todoItem,
+            'done': false
+        });
+        $scope.todoItem = "";
+    }
+    $scope.clearTodo = function() {
+        $scope.todos = $scope.todos.filter(function(item) {
+            return !item.done;
+        })
+        console.log($scope.todos);
+
+    }
+    $scope.clearAll = function() {
+        $scope.todos = [];
+    }
+    $scope.selectAll=function(){
+        flag=true;
+        $scope.todos.forEach(function loop(item){
+            if(!item.done){
+                $scope.todos.forEach(function loop(i){
+                    i.done=true;
+                    flag=false;
+                });
+            }
+        });
+            if(flag){
+                $scope.todos.forEach(function loop(item){
+                    item.done=false;
+                });
+            }
+    }
+    $scope.hide=function(){
+        $scope.todos.forEach(function loop(item){
+            if(!item.done){
+                item.title.style.display="none";
+            }
+        })
+    }
+}]);
